721a457a2fbf90add3d890ef8c904fa616d50af0
Reuben Cleetus <rcleetus@gmail.com>
2020-04-18 23:06:11 -0400

    Added Game Over text

--- spaceinvaders.py
+++ spaceinvaders.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 import math
+from pygame import mixer
 
 # Initialize pygame
 pygame.init()
@@ -11,9 +12,20 @@ font = pygame.font.Font('freesansbold.ttf', 32)
 textX = 10
 textY = 10
 
+#Game Over
+game_over_font = pygame.font.Font('freesansbold.ttf', 64)
+
 def show_score(x, y):
     score = font.render(f"Score : {score_value}", True, (255, 255, 255))
     screen.blit(score, (x, y))
+
+
+#Game Over Message
+def game_over():
+    over_text = game_over_font.render("Game Over", True, (255, 255, 255))
+    screen.blit(over_text, (200, 250))
+    
+
 
 # Create screen
 screen = pygame.display.set_mode((800, 600))
@@ -22,6 +34,10 @@ screen = pygame.display.set_mode((800, 600))
 background = pygame.image.load("./img/background.png")  # Background
 bullet = pygame.image.load("./img/bullet.png")  # bullet
 icon = pygame.image.load("./img/battleship.png")
+
+#Sound
+mixer.music.load('./wav/background.wav')
+mixer.music.play(-1)
 
 # Title and Icon
 pygame.display.set_caption("Space Invaders")
@@ -47,7 +63,7 @@ for i in range(num_of_enemies):
     enemyX.append(random.randint(0, 736))
     enemyY.append(random.randint(50, 150))
     enemyX_change.append(5)
-    enemyY_change.append(5)
+    enemyY_change.append(50)
 #enemyImg = pygame.image.load("./img/enemy.png")
 #enemyX = random.randint(0,736) #Randomize enemy position
 #enemyY = random.randint(50, 150)  # Randomize enemy position
@@ -86,6 +102,7 @@ def fire_bullet(x, y):
     bullet_state = "FIRE"
     screen.blit(bulletImg, (x+16,y+10)) #Make bullet appear on center of ship
 
+
 # Game Loop
 pygame.key.set_repeat(25, 25)
 running = True
@@ -105,13 +122,15 @@ while running:
             playerX += playerX_change
         if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
             if (bullet_state is "READY"):
+                bullet_sound = mixer.Sound('./wav/laser.wav')
+                bullet_sound.play()
                 bulletX = playerX #Grab X value for bullet start point
                 fire_bullet(bulletX, playerY)
 
     #Bullet Movement
     if (bulletY <= 0):
         bulletY = 480
-        bullet_state = "READY"
+        bullet_state = "READY" #Reset bullet, since it exited the screen
     if (bullet_state is "FIRE"):
         fire_bullet(bulletX, bulletY) #Use bullet start X coordinate
         bulletY -= bulletY_change
@@ -124,6 +143,13 @@ while running:
     
     # Enemy Movement
     for i in range(num_of_enemies):
+        #Game Over Code
+        if enemyY[i] > 440:
+            for j in range(num_of_enemies):
+                enemyY[j] = 2000  #Move enemies off screen
+            game_over()
+            break
+
         enemyX[i] += enemyX_change[i]
         if (enemyX[i] <= 0):
             enemyX_change[i] = 5
@@ -135,6 +161,8 @@ while running:
         #Collision
         collision = isCollision(enemyX[i], enemyY[i], bulletX, bulletY)
         if (collision):
+            collision_sound = mixer.Sound('./wav/explosion.wav')
+            collision_sound.play()
             bulletY = 480
             bullet_state = "READY"
             score_value += 100
